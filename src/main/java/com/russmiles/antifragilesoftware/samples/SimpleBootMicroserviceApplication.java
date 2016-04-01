dade0b84b2fafa64d3c61b185f754a0b8e641cc2
Russ Miles <russ@russmiles.com>
2016-04-01 11:40:31 -0400

    First configured version of service

---
+++ src/main/java/com/russmiles/antifragilesoftware/samples/SimpleBootMicroserviceApplication.java
@@ -0,0 +1,23 @@
+package com.russmiles.antifragilesoftware.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class SimpleBootMicroserviceApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return this.toString() + " instance saying: Hello Microservice World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBootMicroserviceApplication.class, args);
+    }
+}
